552542ad8c1efe2ff6c2c9668a6017a523a0a60a
gabriele <gabrielescodinu96@gmail.com>
2020-11-27 16:09:30 +0100

    commit 02

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
 var next = $(".next i");
 var prev = $(".prev i");
-var imageNext = $(".last");
-var imagePrev = $(".active")
 
-
+// funzione click di destra, quindi next
 next.click(function(){
-  imageNext.show();
-  imagePrev.hide();
+  // assegno ad una variabile l'immagine attiva
+  var immagineAttiva = $("img.active");
+
+  // rimuovo dall'immagine la classe active,
+  // quindi l'immagine non è più attiva
+  immagineAttiva.removeClass("active");
+
+  // assegno all'immagine fratello successiva di destra
+  // (con .next) la classe active.
+  immagineAttiva.next().addClass("active");
+
+  // faccio la stessa cosa per i tre puntini blu
+  var cerchioAttivo = $("i.active");
+  cerchioAttivo.removeClass("active");
+  cerchioAttivo.next().addClass("active");
 })
 
+// funzione click di sinistra, quindi prev
 prev.click(function(){
-  imagePrev.show();
-  imageNext.hide();
+  var immagineAttiva = $("img.active");
+
+  immagineAttiva.removeClass("active");
+  // assegno all'immagine fratello precedente di sinistra
+  // (con .next) la classe active.
+
+  immagineAttiva.prev().addClass("active");
+
+  // faccio la stessa cosa per i tre puntini blu
+  var cerchioAttivo = $("i.active");
+  cerchioAttivo.removeClass("active");
+  cerchioAttivo.prev().addClass("active");
 })
