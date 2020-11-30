71f5d47bb66c3e44dee08e9c0029e5f92077823c
gabriele <gabrielescodinu96@gmail.com>
2020-11-30 10:17:17 +0100

    commit 03

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var next = $(".next i");
 var prev = $(".prev i");
 
+
 // funzione click di destra, quindi next
 next.click(function(){
   // assegno ad una variabile l'immagine attiva
@@ -18,20 +19,40 @@ next.click(function(){
   var cerchioAttivo = $("i.active");
   cerchioAttivo.removeClass("active");
   cerchioAttivo.next().addClass("active");
-})
+
+  // usa if per far riniziare il ciclo delle immagini.
+  if (immagineAttiva.hasClass("last")) {
+    immagineAttiva = $("img.first")
+    immagineAttiva.addClass("active");
+  }
+
+  // stessa cosa per i puntini.
+  if (cerchioAttivo.hasClass("last")) {
+    cerchioAttivo = $("i.first")
+    cerchioAttivo.addClass("active");
+  }
+});
+
 
 // funzione click di sinistra, quindi prev
 prev.click(function(){
   var immagineAttiva = $("img.active");
 
   immagineAttiva.removeClass("active");
-  // assegno all'immagine fratello precedente di sinistra
-  // (con .next) la classe active.
 
   immagineAttiva.prev().addClass("active");
 
-  // faccio la stessa cosa per i tre puntini blu
   var cerchioAttivo = $("i.active");
   cerchioAttivo.removeClass("active");
   cerchioAttivo.prev().addClass("active");
-})
+
+  if (immagineAttiva.hasClass("first")) {
+    immagineAttiva = $("img.last")
+    immagineAttiva.addClass("active");
+  }
+
+  if (cerchioAttivo.hasClass("first")) {
+    cerchioAttivo = $("i.last")
+    cerchioAttivo.addClass("active");
+  }
+});
